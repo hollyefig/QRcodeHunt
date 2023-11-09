9f90c3b795fc07ae5aac62bc6811d48b0ee1f2c7
mts_dev3 <mts_dev3@stephensdirect.com>
2023-11-09 15:24:42 -0500

    added stage2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const keyCode = [3, 5, 2, 9],
+  currentUrl = window.location.href,
   titleText = "QR Code Hunt!".split(""),
   colors = {
     dark: "#0e100f",
@@ -9,10 +10,11 @@ const keyCode = [3, 5, 2, 9],
   },
   topBarHeight = 100;
 
-keyCode.forEach((e) => {
+keyCode.forEach((e, index) => {
   let num = document.createElement("div");
   let underline = document.createElement("div");
-  num.setAttribute("class", "num");
+  num.setAttribute("class", `num_${index} opacity0`);
+
   underline.setAttribute("class", "numUnderline");
   num.textContent = e;
   document.querySelector(".keyWrapper").append(num, underline);
@@ -28,10 +30,21 @@ titleText.forEach((e) => {
   }
 });
 
-// default timeline
-let timeline = gsap.timeline({
-  defaults: { delay: 1, duration: 0.5, ease: "power2" },
-});
-timeline
-  .from(".topBar", { y: -topBarHeight })
-  .from(".title > span", { opacity: 0, stagger: 0.1, delay: 0 });
+console.log(currentUrl.includes("stage2"));
+
+if (currentUrl.includes("stage2")) {
+  let timeline = gsap.timeline({
+    defaults: { delay: 1, duration: 0.5, ease: "power2" },
+  });
+  timeline
+    .to(".num_0", { opacity: 1 })
+    .from(".topBar", { height: "100vh", duration: 0.7 });
+} else {
+  // default timeline
+  let timeline = gsap.timeline({
+    defaults: { delay: 1, duration: 0.5, ease: "power2" },
+  });
+  timeline
+    .from(".topBar", { y: -topBarHeight })
+    .from(".title > span", { opacity: 0, stagger: 0.1, delay: 0 });
+}
