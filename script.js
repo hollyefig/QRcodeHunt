20836e12ed0d417aa76a2febec0e247e6efc561b
mts_dev3 <mts_dev3@stephensdirect.com>
2023-11-09 14:53:07 -0500

    index setup

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// func to grab url
-const getCurrentURL = () => {
-  return window.location.href;
-};
-const url = getCurrentURL(),
-  keyCode = [3, 5, 7, 9],
-  ul = document.querySelector(".keySet");
+const keyCode = [3, 5, 2, 9],
+  titleText = "QR Code Hunt!".split(""),
+  colors = {
+    dark: "#0e100f",
+    mediumDark: "#3b438f",
+    normal: "#d556fa",
+    mediumLight: "#8de6e9",
+    light: "#fefce1",
+  },
+  topBarHeight = 100;
 
-const addKey = (n) => {
-  document.querySelector(`.addKey${n}`).href = `${url}#${keyCode[n - 1]}`;
-  ul.children[n - 1].textContent = keyCode[n - 1];
-};
+keyCode.forEach((e) => {
+  let num = document.createElement("div");
+  let underline = document.createElement("div");
+  num.setAttribute("class", "num");
+  underline.setAttribute("class", "numUnderline");
+  num.textContent = e;
+  document.querySelector(".keyWrapper").append(num, underline);
+});
 
-window.onload = function () {
-  console.log("current", getCurrentURL());
-};
+titleText.forEach((e) => {
+  let span = document.createElement("span");
+  if (e === " ") {
+    document.querySelector(".title").appendChild(document.createElement("br"));
+  } else {
+    span.textContent = e;
+    document.querySelector(".title").appendChild(span);
+  }
+});
+
+// default timeline
+let timeline = gsap.timeline({
+  defaults: { delay: 1, duration: 0.5, ease: "power2" },
+});
+timeline
+  .from(".topBar", { y: -topBarHeight })
+  .from(".title > span", { opacity: 0, stagger: 0.1, delay: 0 });
