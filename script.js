8aee51570d484a4ca82ef039a3d89398ebb035de
mts_dev3 <mts_dev3@stephensdirect.com>
2023-11-11 09:53:21 -0500

    changed key number input

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ const keyCode = [3, 5, 2, 9],
   wrapper = document.querySelector(".wrapper"),
   questionWrapper = document.querySelector(".questionWrapper");
 
-console.log();
-
 // KEY CODE SETUP AT TOPBAR
 keyCode.forEach((e, index) => {
-  let num = document.createElement("div");
+  let numWrapper = document.createElement("div");
+  let num = document.createElement("span");
   let underline = document.createElement("div");
-  num.setAttribute("class", `num_${index}`);
+  numWrapper.setAttribute("class", `num_${index}`);
   num.style.opacity = 0;
 
   underline.setAttribute("class", "numUnderline");
   num.textContent = e;
-  document.querySelector(".keyWrapper").append(num, underline);
+  document.querySelector(".keyWrapper").append(numWrapper, underline);
+  numWrapper.appendChild(num);
 });
 
 // PAGE LOAD ANIMATIONS, ASSIGN ATTRIBUTE
+
 let timeline = gsap.timeline({
   defaults: { delay: 1, duration: 0.5, ease: "power2" },
 });
@@ -36,29 +37,41 @@ const scaleExpand = { scale: 2 },
   scaleDefault = { scale: 1, delay: 0, duration: 0.4 },
   topBarSet = { height: "100vh", duration: 0.7, delay: 0.3 };
 
+const enterKey = (e) => {
+  timeline
+    .to(`.num_${e} > span`, { opacity: 1, delay: 0 })
+    .fromTo(`.num_${e} > span`, scaleExpand, scaleDefault, "<");
+  document.querySelector(`.num_${e}`).classList.remove("numBgFlash");
+};
+
 for (let i = 0; i < stages.length; i++) {
   if (stages[i] !== "stage1") {
     if (currentUrl.includes(stages[i])) {
       //set wrapper name
       wrapper.setAttribute("id", stages[i]);
-      let n = stages.indexOf(stages[i]) - 1;
+      let n = stages.indexOf(stages[i]) - 1,
+        currentNum = document.querySelector(`.num_${n}`);
       // Add in current keycodes entered
-      console.log("get length", i);
       if (i > 1) {
         for (let k = 0; k < i - 1; k++) {
-          document.querySelector(`.num_${k}`).style.opacity = 1;
+          document.querySelector(`.num_${k} > span`).style.opacity = 1;
+          document
+            .querySelector(`.num_${k} > span`)
+            .removeAttribute("onclick", `enterKey(${k})`);
         }
       }
-      timeline
-        .to(`.num_${n}`, { opacity: 1 })
-        .fromTo(`.num_${n}`, scaleExpand, scaleDefault, "<")
-        .from(".topBar", topBarSet)
-        .to(
-          "body",
-          { backgroundPosition: "0px -190px", delay: 0, duration: 1.5 },
-          "<"
-        )
-        .to(".questionWrapper", { display: "flex", opacity: 1, y: -20 }, "<");
+      currentNum.classList.add("numBgFlash");
+      currentNum.setAttribute("onclick", `enterKey(${n})`);
+      // timeline
+      //   .to(`.num_${n}`, { opacity: 1 })
+      //   .fromTo(`.num_${n}`, scaleExpand, scaleDefault, "<")
+      //   .from(".topBar", topBarSet)
+      //   .to(
+      //     "body",
+      //     { backgroundPosition: "0px -190px", delay: 0, duration: 1.5 },
+      //     "<"
+      //   )
+      //   .to(".questionWrapper", { display: "flex", opacity: 1, y: -20 }, "<");
     }
   } else {
     if (!currentUrl.includes("stages")) {
@@ -102,7 +115,7 @@ const proceed = () => {
     )
     .to(".proceed, .title", { display: "none", duration: 0 }, "<.5")
     .to(".questionWrapper", { display: "flex", duration: 0 }, "<")
-    .to(".questionWrapper", { opacity: 1, y: -20 }, "<");
+    .from(".questionWrapper", { opacity: 0, y: 40 }, "<");
 };
 
 // BLUR BG, QUESTION ANSWER REVEAL
@@ -114,7 +127,7 @@ const selectAnswer = (e) => {
   document.querySelector(".blurBgMsgWrapper").style.display = "flex";
   gsap.timeline().to(".blurBgMsgWrapper", {
     opacity: 1,
-    duration: 0.5,
+    duration: 0.3,
     delay: 0.1,
     scale: 1,
   });
